3fb53aad9e3536fe99a14b0662237446c2314977
amit <amitsarker95@gmail.com>
2024-11-08 11:36:50 +0600

    Beautifulsoup added for extract links from html

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from dotenv import load_dotenv
 import imaplib
 import email
+from bs4 import BeautifulSoup
 import os
 load_dotenv()
 
 
 username = os.getenv('Email')
 password = os.getenv('PASSWORD')
-
 
 def connect_email():
     mail = imaplib.IMAP4_SSL("imap.gmail.com")
@@ -17,10 +17,19 @@ def connect_email():
     return mail
 
 
+def extract_links_from_html(html_content):
+    soup = BeautifulSoup(html_content, "html.perser")
+    links = [link["href"] for link in soup.findall("a", href=True) if "unsubscribe" in link["href"].lower()]
+
+    return links
+
+
 def search_email():
     mail = connect_email()
     _, search_data = mail.search(None, '(BODY "unsubscribe")')
     data = search_data[0].split()
+
+    links = []
 
     for num in data:
         _, data = mail.fetch(num, "(RFC822)")
@@ -30,13 +39,18 @@ def search_email():
             for part in msg.walk():
                 if part.get_content_type() == "text/html":
                     html_content = part.get_payload(decode=True).decode()
-                    print(html_content)
+                    links.extend(extract_links_from_html(html_content))
+
         else:
             content_type = msg.get_content_type()
             content = msg.get_payload(decode=True).decode()
 
             if content_type == "text/html":
-                print(content)
+                links.extend(extract_links_from_html(content))
 
 
     mail.logout()
+    return links
+
+links = search_email()
+print(links)
